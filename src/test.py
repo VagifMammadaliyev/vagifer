f5f5d690c30e1a967cb718960f0364789e8ffaa9
VagifMammadaliyev <vagifmammadaliyev@outlook.com>
2019-05-23 03:17:53 +0400

    fix cellsize in test code

--- src/test.py
+++ src/test.py
@@ -6,5 +6,5 @@ size = (500, 500)
 cellsize = (10, 10)
 size_in_cells = (size[0] // cellsize[0], size[1] // cellsize [1])
 create_gif('hypno', size=size,
-           cellsize=(8, 8), frames=30,
+           cellsize=(10, 10), frames=30,
            painter=HypnoticPainter(size_in_cells, colorized=True))
